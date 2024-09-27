cb3738e7f739548f9bde0be25ed92251db4f7a5a
myrs <6205312+myrshfrs@users.noreply.github.com>
2024-09-28 00:08:20 +0800

    Update workers.js

--- workers.js
+++ workers.js
@@ -1,7 +1,7 @@
 let geolite2_country = null;
 let CLOUDFLARE_API_TOKEN = null;
 // const dohUrl = 'https://dns.google/dns-query';
-const dohUrl = 'https://dns.nextdns.io/78826c';
+const dohUrl = 'https://ndhaue.zhangwf.eu.org/1:ABEAATAQ';
 // const dohUrl = 'https://unfiltered.adguard-dns.com/dns-query';
 
 export default {
